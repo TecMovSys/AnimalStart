86f7e350e514582c609e8574749092ec8808fdf3
CShinoDev <davidqo197@gmail.com>
2025-08-28 13:39:13 -0500

    first commit frontend

---
+++ cliente/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola bienvenido a Animal Start</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
